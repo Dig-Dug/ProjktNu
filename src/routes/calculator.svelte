742459f40a85198b02dc9bd7a12bc369f7144df4
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-11-03 18:29:49 +0100

    calc

--- src/routes/calculator.svelte
+++ src/routes/calculator.svelte
@@ -1,95 +1,35 @@
 <script>
-	let three = 3;
-	let num = 0;
-	function sum() {
-		return num + num;
-	}
-	function result() {
-		console.log(sum);
-		sum();
-	}
-	function numb() {
-		console.log(num);
+	//import Keypad from './Keypad.svelte';
+	import Keypad from './todos/Keypad.svelte';
+
+	let pin;
+	//$: view = pin ? pin.replace(/\d(?!$)/g, '•') : '';
+	$: view = pin;
+	function handleSubmit() {
+		alert(`submitted ${pin}`);
 	}
 </script>
 
 <h1>The Svelte Calculator</h1>
-
-<div class="digits">
-	<input
-		type="button"
-		on:click={() => {
-			num = 1;
-			console.log(num);
-			num += num;
-		}}
-		value="1"
-	/>
-	<input
-		type="button"
-		on:click={() => {
-			num = 2;
-		}}
-		value="2"
-	/>
-	<input type="button" on:click={numb} bind:value={three} />
-	<input type="button" value="4" />
-	<input type="button" value="5" />
-	<input type="button" value="6" />
-	<input type="button" value="7" />
-	<input type="button" value="8" />
-	<input type="button" value="9" />
-	<input type="button" value="." />
-	<input type="button" value="0" />
-	<input
-		type="button"
-		on:click={() => {
-			num = 0;
-			console.log(num);
-		}}
-		value="C"
-	/>
+<div class="center">
+	<Keypad bind:value={pin} on:submit={handleSubmit} />
+	<h1>{view}</h1>
 </div>
-
-<div class="operators">
-	<button on:click={sum}>+</button>
-	<button>-</button>
-	<button>*</button>
-	<button>/</button>
-	<button on:click={result}>=</button>
-</div>
-<div class="inputz">
-	<input type="number" bind:value={num} />
-</div>
-
-<a href="/">back</a>
+<label>
+	<a href="/">back</a>
+</label>
 
 <style>
+	.center {
+		left: 38%;
+		position: relative;
+	}
 	a {
+		left: 38%;
+		position: relative;
 		background-color: orange;
 	}
-	.digits {
-		widows: 418px;
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		grid-template-rows: repeat(4, 1fr);
-		grid-column-gap: 4px;
-		grid-row-gap: 6px;
-	}
-	.inputz {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		grid-template-rows: 1fr;
-		grid-column-gap: 24px;
-		grid-row-gap: 0px;
-	}
-	.operators {
-		width: 135px;
-		margin-left: 124px;
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		grid-template-rows: repeat(2, 1fr);
-		grid-column-gap: 3px;
-		grid-row-gap: 2px;
+	h1 {
+		position: sticky;
 	}
 </style>
---
+++ src/routes/todos/Keypad.svelte
@@ -0,0 +1,59 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let value = '';
+
+	const dispatch = createEventDispatcher();
+
+	const select = (num) => () => (value += num);
+	const clear = () => (value = '');
+	const submit = () => dispatch('submit');
+	let sum = () => {
+		console.log(value + value);
+		return (value += value);
+	};
+</script>
+
+<div class="keypad">
+	<button on:click={select(1)}>1</button>
+	<button on:click={select(2)}>2</button>
+	<button on:click={select(3)}>3</button>
+	<button on:click={select(4)}>4</button>
+	<button on:click={select(5)}>5</button>
+	<button on:click={select(6)}>6</button>
+	<button on:click={select(7)}>7</button>
+	<button on:click={select(8)}>8</button>
+	<button on:click={select(9)}>9</button>
+
+	<button disabled={!value} on:click={clear}>C</button>
+	<button on:click={select(0)}>0</button>
+	<button disabled={!value} on:click={submit}>=</button>
+	<div class="op">
+		<button disabled={!value} on:click={sum}>+</button>
+		<button disabled={!value} on:click={submit}>-</button>
+		<button disabled={!value} on:click={submit}>*</button>
+		<button disabled={!value} on:click={submit}>/</button>
+	</div>
+</div>
+
+<style>
+	.keypad {
+		display: grid;
+		grid-template-columns: repeat(3, 5em);
+		grid-template-rows: repeat(4, 3em);
+		grid-gap: 0.5em;
+	}
+
+	button {
+		margin: 0;
+	}
+	.op {
+		display: grid;
+		grid-template-columns: repeat(2, 5em);
+		grid-template-rows: repeat(3, 3em);
+		grid-gap: 0.5em;
+		position: absolute;
+		left: 30%;
+		color: rgb(37, 89, 185);
+	}
+</style>
