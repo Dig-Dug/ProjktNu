6dbbbae86ccb8629a6ae3873f6b9e4951c6affa4
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-12-02 16:58:14 +0100

    calc from Wahhab Baldwin

--- src/routes/calculator2.svelte
+++ src/routes/calculator2.svelte
@@ -1,6 +1,45 @@
 <script>
-    import Calcbtn from "./todos/calc/Calcbtn.svelte";
-    import
+	import Calcbtn from './todos/calc/Calcbtn.svelte';
+	import Display from './todos/calc/display.svelte';
+
+	let calc;
 </script>
 
-<h1>lol</h1>
+<h1>Props to --> https://github.com/wahhabb</h1>
+<div class="calc" bind:this={calc}>
+	<Display />
+	<Calcbtn use="fn">AC</Calcbtn>
+	<Calcbtn use="plusminus"><sup>&plus;</sup>/<sub>&minus;</sub></Calcbtn>
+	<Calcbtn use="fn">%</Calcbtn>
+	<Calcbtn use="oper">&div</Calcbtn>
+	<Calcbtn>7</Calcbtn>
+	<Calcbtn>8</Calcbtn>
+	<Calcbtn>9</Calcbtn>
+	<Calcbtn use="oper">&times;</Calcbtn>
+	<Calcbtn>4</Calcbtn>
+	<Calcbtn>5</Calcbtn>
+	<Calcbtn>6</Calcbtn>
+	<Calcbtn use="oper">&minus;</Calcbtn>
+	<Calcbtn>1</Calcbtn>
+	<Calcbtn>2</Calcbtn>
+	<Calcbtn>3</Calcbtn>
+	<Calcbtn use="oper">&plus;</Calcbtn>
+	<Calcbtn>0</Calcbtn>
+	<Calcbtn>.</Calcbtn>
+	<Calcbtn use="oper">=</Calcbtn>
+	<a href="/">back</a>
+</div>
+
+<style>
+	a {
+		background-color: orange;
+	}
+	.calc {
+		display: inline-grid;
+		justify-content: center;
+		grid-template-columns: repeat(3, 3.875em) 4.06em;
+		grid-template-rows: 5em repeat(5, 3.125em);
+		margin: 0 auto;
+		gap: 0.125em;
+	}
+</style>
--- src/routes/todos/calc/Calcbtn.svelte
+++ src/routes/todos/calc/Calcbtn.svelte
@@ -1,7 +1,96 @@
-<script>
-	export let use = 'number';
+<script context="module">
+	let lastBtn = ''; //context module exports everything here
+	let inDecimal = 0;
+	let lastOper = '';
+	let operand = '';
 </script>
 
-<button class={use}>
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { display } from './stores';
+	export let use = 'number';
+	const dispatch = createEventDispatcher();
+	const calcClick = (a) => {
+		const btn = a.target.innerHTML;
+		if (use == 'plusminus') {
+			// +/- key
+			display.set(-$display);
+			return 0;
+		}
+		if (btn == '%') {
+			display.set($display / 100);
+			return 0;
+		}
+		if ('0' <= btn && btn <= '9') {
+			if (lastBtn != 'number') {
+				display.set(0);
+			}
+			if (inDecimal == 1) {
+				display.set(Number(String($display) + '.' + btn));
+				++inDecimal;
+			} else {
+				if (inDecimal) {
+					display.set(Number(String($display) + btn));
+					++inDecimal;
+				} else {
+					display.set($display * 10 + Number(btn));
+				}
+			}
+			lastBtn = 'number';
+		} else {
+			switch (btn) {
+				case '.':
+					if (inDecimal == 0) {
+						inDecimal = 1;
+					}
+					break;
+				case 'AC':
+					lastOper = ''; // fall through!
+				case 'C':
+					display.set(0);
+					lastBtn = 'number';
+					a.target.innerHTML = 'AC';
+					inDecimal = 0;
+					break;
+				case '+':
+				case '\u2212': // Minus
+				case '\u00D7': // Multiply
+				case '\u00F7': // Divide
+				case '=':
+					// dispatch('func', {
+					// 	symbol: btn,
+					// });
+					switch (lastOper) {
+						case '':
+							operand = $display;
+							break;
+						case '+':
+							operand += $display;
+							break;
+						case '\u2212': // Minus
+							operand -= $display;
+							break;
+						case '\u00D7': // Multiply
+							operand *= $display;
+							break;
+						case '\u00F7': // Divide
+							operand /= $display;
+							break;
+					}
+					display.set(operand);
+					lastBtn = 'operator';
+					lastOper = btn;
+					inDecimal = 0;
+					if (btn === '=') {
+						lastOper = '';
+						operand = 0;
+					}
+					break;
+			}
+		}
+	};
+</script>
+
+<button class={use} on:click={(a) => calcClick(a)}>
 	<slot />
 </button>
