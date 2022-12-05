bfa3fc668d18a132aca75bf0c64431477975ff40
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-12-05 15:51:27 +0100

    flashcardz

--- src/routes/flashcard.svelte
+++ src/routes/flashcard.svelte
@@ -1,5 +1,27 @@
+<script>
+	import Card from './todos/card.svelte';
+
+	let open = false;
+	function addCard() {
+		open = true;
+		return;
+	}
+	function delCard() {
+		open = false;
+	}
+	function whot() {
+		console.log('kioppoi');
+	}
+</script>
+
 <h1>Flashcards</h1>
+<button on:click={addCard}>{open ? '' : ''}Add Card</button>
+<button on:click={delCard}>Delete Cards</button>
 <a href="/">back</a>
+
+{#if open}
+	<Card on:click={whot} />
+{/if}
 
 <style>
 	a {
---
+++ src/routes/todos/card.svelte
@@ -0,0 +1,37 @@
+<script>
+	/* let pp = 'sr'; */
+	let question = '';
+	let answer = '';
+	let flashcard = false;
+	let showIt = false;
+	function save() {
+		localStorage.setItem(question, '<--');
+		localStorage.setItem(answer, '<--');
+		flashcard = true;
+	}
+	function close() {}
+	function show() {
+		showIt = true;
+	}
+</script>
+
+<!-- {#each pp as i} -->
+<div>
+	<h1>Create Flashcard</h1>
+	<textarea name="ziuzuizu" id="" cols="30" rows="10" bind:value={question} />
+	<textarea name="" id="" cols="30" rows="10" bind:value={answer} />
+	<button on:click={save}>Save</button>
+	<button on:click={close}>Close</button>
+	<!-- <button>Close {i}</button> -->
+</div>
+
+{#if flashcard}
+	<div on:click={show}>
+		<p>{question}</p>
+		{#if showIt}
+			<p>{answer}</p>
+		{/if}
+	</div>
+{/if}
+
+<!-- {/each} -->
