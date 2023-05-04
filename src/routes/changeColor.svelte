a5468e906cec5e613b2d86618e5c87d86d8bf5c5
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-05-04 10:30:12 +0200

    2023 New Year

--- src/routes/changeColor.svelte
+++ src/routes/changeColor.svelte
@@ -1,57 +1,47 @@
 <script>
+	function changeColor() {
+		let i = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let ii = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let iii = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
 
-function changeColor ()  {
-	let i =   Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	let ii =   Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	let iii =   Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	
-	let rgb = "rgb";
+		let rgb = 'rgb';
 
-	
-	let l  = rgb + "(" +(i   + " " + ii  + " "+ iii  )+ ")";
-	document.body.style.backgroundColor = l;
-}
+		let l = rgb + '(' + (i + ' ' + ii + ' ' + iii) + ')';
+		document.body.style.backgroundColor = l;
+	}
 
-	 function back(){
-		document.body.style.backgroundColor = "white";
-	 }
-	
-	
-	</script>
-	
-	
-	
-	
-	
-	<button on:click={changeColor}>Change Background Color</button>
-	<a  href="/" on:click={back}>back</a>
-	
-	
-	
-	
-	<style>
-	
-	
-	
-	
-	button{
-	   background-color: rgb(255, 254, 251);
-		  width: 112px;border-radius: 4px;
-	   position: relative; 
-	   top: 40px;
-	   left:390px;
+	function back() {
+		document.body.style.backgroundColor = 'white';
 	}
-	button:hover{
-background-color: black;
-color: aliceblue;
+</script>
+
+<button on:click={changeColor}>Change Background Color</button>
+<a href="/" on:click={back}>back</a>
+
+<style>
+	button {
+		background-color: rgb(255, 254, 251);
+		width: 112px;
+		border-radius: 4px;
+		position: relative;
+		top: 40px;
+		left: 390px;
 	}
-	 a{position: relative;
-		border-radius: 4px;  width: 36px; top:100px;
-	background-color: rgba(220, 20, 60, 0.486);
-		 height: 23px;
-	
+	button:hover {
+		background-color: black;
+		color: aliceblue;
 	}
-	label{
-	   position: relative;top: 200px;left:300px;
+	a {
+		position: relative;
+		border-radius: 4px;
+		width: 36px;
+		top: 100px;
+		background-color: rgba(220, 20, 60, 0.486);
+		height: 23px;
 	}
-	</style>
+	label {
+		position: relative;
+		top: 200px;
+		left: 300px;
+	}
+</style>
--- src/routes/gradientPicker.svelte
+++ src/routes/gradientPicker.svelte
@@ -1,74 +1,71 @@
 <script>
+	let rgb = 'rgb';
 
+	let lg = 'linear-gradient(';
 
-let rgb = "rgb";
+	let z = '';
+	function getGradient() {
+		console.log('wwww');
+		let i = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		z = document.body.style.background =
+			lg + '180deg, ' + rgb + '(' + r + ', ' + g + ' ,' + b + ')' + ',' + '#' + r + ')';
 
-
-let lg = "linear-gradient(";
-
-let z = ""; 
-function getGradient(){
-   console.log("wwww")
-   let i = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-   let  r  = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-let  g =  Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-let  b =  Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-  z =  document.body.style.background = lg + "180deg, " +  rgb + "(" + r + ", " + g + " ," + b + ")" + "," + "#" + r + ")";
-
-return;
-}
-function back(){
-		document.body.style.background = "white";
-	 }
-
-
+		return;
+	}
+	function back() {
+		document.body.style.background = 'white';
+	}
 </script>
+
 <h1>Gradient Picker</h1>
 <div class="center">
-	
+	<button on:click={getGradient} on:mouseover={getGradient} on:focus={getGradient}
+		>Generate Gradient</button
+	>
 
-	<button   on:click={getGradient} on:mouseover={getGradient} on:focus={getGradient}>Generate Gradient</button>
+	<p class="poi">{z}</p>
 
-<p class="poi">{z}</p>
-
-
-
-<a  href="/" on:click={back}>back</a>
-
+	<a href="/" on:click={back}>back</a>
 </div>
 
 <style>
-   .center{
-      margin-left: 37%;
-    margin-right: 38%;
-
-   }
-   .poi{
-      padding-bottom: 13px;
-    padding-top: 14px;
-   }
-button{
-	   background-color: rgb(255, 254, 251);
-		  width: 112px;border-radius: 4px;
-	   position: relative; 
-	   top: 40px;
-	   margin: auto;
+	.center {
+		margin-left: 37%;
+		margin-right: 38%;
 	}
-	button:hover{
-background-color: black;
-color: aliceblue;
+	.poi {
+		padding-bottom: 13px;
+		padding-top: 14px;
 	}
-   a{ 
-      position: relative;
-      border-radius: 4px;  width: 36px; top:44px;
-	background-color: rgba(220, 20, 60, 0.486);    
-   }
-   p{
-      background-color: rgb(255, 255, 255);
-      font-size: 34px;
-      margin: auto;
-      margin-top: 124px;
-      padding: 67px;height: opx;    
-      border-radius: 39px;
-   }
+	button {
+		background-color: rgb(255, 254, 251);
+		width: 112px;
+		border-radius: 4px;
+		position: relative;
+		top: 40px;
+		margin: auto;
+	}
+	button:hover {
+		background-color: black;
+		color: aliceblue;
+	}
+	a {
+		position: relative;
+		border-radius: 4px;
+		width: 36px;
+		top: 44px;
+		background-color: rgba(220, 20, 60, 0.486);
+	}
+	p {
+		background-color: rgb(255, 255, 255);
+		font-size: 34px;
+		margin: auto;
+		margin-top: 124px;
+		padding: 67px;
+		height: opx;
+		border-radius: 39px;
+	}
 </style>
