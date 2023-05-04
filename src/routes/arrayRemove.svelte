181eb3051dbeaca2b80f5a6b0d465fcae3425d47
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-05-04 10:14:38 +0200

    2023 New Year

--- src/routes/arrayRemove.svelte
+++ src/routes/arrayRemove.svelte
@@ -2,24 +2,39 @@
 	//Remove First and Last Character Part Two
 	import { onMount } from 'svelte';
 	let re = 0;
+	let r = 10;
 
+	let ree = document.createElement('p');
 	function gen() {
 		let r = Math.floor(Math.random() * 10) + 3;
 		for (let i = 0; i < 10; i++) {
 			re = r;
-			console.log(r);
+			let rrr = parseInt(i);
+
 			onMount(() => {
-				let re = document.createElement('p');
+				let ree = document.createElement('p');
 				//re.innerText = `${re}`;
-				document.body.appendChild(re);
+				ree.innerHTML = 'ee';
+
+				document.body.appendChild(ree);
+				console.log(ree, re);
+				//listNode.appendChild(re);
+				return ree;
 			});
-			return;
+			//console.log(r, rrr);
+			//return;
 		}
 	}
+	/* function gen() {
+		return (re = 10);
+	} */
+	//document.body.appendChild(ree);
 </script>
 
 <p>remove first and last element of array</p>
 <button on:click={gen}>elements generator</button>
 {#if re}
-	<p bind:this={re} />
+	<p bind:this={re}>{re}</p>
+	<!--<p>{re} * {re}</p>-->
+	<p>{ree} *= {re}</p>
 {/if}
