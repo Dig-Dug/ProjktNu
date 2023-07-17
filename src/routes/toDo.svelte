145c9c396584dc7d42bc95756ab0beee5cc39b98
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-07-17 12:00:35 +0200

    2023 New Year

--- src/routes/toDo.svelte
+++ src/routes/toDo.svelte
@@ -1,20 +1,30 @@
 <script>
+	import { beforeUpdate, afterUpdate } from 'svelte';
 	let message = '';
+
 	let open = false;
 	function display() {
+		//https://bobbyhadz.com/blog/javascript-clear-input-field-after-submit
+
 		open = true;
+
 		return;
 	}
 	function deleteThis() {
 		message = '';
 		open = false;
 	}
+
+	let src = 'easter.jpg';
+	let pic = true;
 </script>
 
+r
 <!-- <svelte:head>
 	<Todo ink href="./todo.png" />
 </svelte:head> -->
 <h1>To-Do List 🗿</h1>
+<img class:pic {src} alt="w" />
 <div class="au">
 	<textarea name="" id="" cols="33" rows="1" bind:value={message} />
 	<button on:click={display}>submit</button>
@@ -29,6 +39,7 @@
 		{message}
 		{@html message}
 		<input type="checkbox" />
+		<p style="cursor: pointer;" on:click={deleteThis}>🩻</p>
 	</div>
 {/if}
 <a class="s" href="/">back</a>
@@ -52,5 +63,13 @@
 		justify-content: center;
 		align-items: center;
 		background-color: aqua;
+	}
+	.pic {
+		height: 1114px;
+		width: 1934px;
+		position: absolute;
+		z-index: -1;
+		top: 0px;
+		bottom: 95px;
 	}
 </style>
