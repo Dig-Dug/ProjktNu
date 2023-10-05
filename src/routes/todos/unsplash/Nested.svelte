7d8c5a0ad84da5f41e74e37b50ef3dc0a182847c
felipe loewenstein <comandodada@hotmail.com>
2023-10-05 12:27:44 +0200

    fetchhhh

---
+++ src/routes/todos/unsplash/Nested.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let onClose;
+	import { onMount, onDestroy } from 'svelte';
+
+	function onClickDocument(e) {
+		onClose();
+	}
+	onMount(() => {
+		document.addEventListener('click', onClickDocument);
+	});
+	onDestroy(() => {
+		document.removeEventListener('click', onClickDocument);
+	});
+</script>
--- src/routes/unsplash.svelte
+++ src/routes/unsplash.svelte
@@ -2,18 +2,33 @@
 	//https://dig-dug.github.io/funprjkts.github.io/unsplash/index.html
 	import { onMount } from 'svelte';
 
+	//	import Nested from './todos/unsplash/Nested.svelte';
 	/* 
 	onMount(() => {
 		const myImage = document.querySelector('img');
 	}); */
 
-	onMount(async () => {
+	/* onMount(async () => {
 		//	fetchImage;
 
 		console.log('on');
 	});
-
+ */
 	let rr = false;
+	let rrr = false;
+	// @ts-ignore
+	const lets = (event) => {
+		if ((event = lets)) {
+			//rrr = true;
+			//go();
+			console.log(rr, '🍥');
+		}
+	};
+	onMount(() => {
+		window.addEventListener('click', lets);
+		//go();
+		return () => window.removeEventListener('click', lets);
+	});
 
 	//For Random images
 	let src = '...';
@@ -32,33 +47,63 @@
 		return await response.json();
 	})();
 
+	////////////
+	let src1 = '...';
+	let API1 = 'https://picsum.photos/300';
+	fetch(API1).then((res) => res.url);
+	async function loadTT() {
+		const res = await fetch(API1);
+		src1 = await res.url;
+	}
+	loadTT();
+	//async function fetchImage() {}
+
 	function go() {
 		rr = true;
-
+		loadT();
+		loadTT();
 		/* setTimeout(() => {
 			return (rr = false);
 		}, 5000); */
 	}
+	/* 	function onClose() {
+		rr = false;
+	}
+	function onClickD(e) {
+		console.log('kokpeofkwopekfopwekp');
+		onClose();
+	} */
 </script>
 
 <div class="center">
 	<h1>Unsplash</h1>
 	<input type="text" name="search" id="" />
-	<!-- <button on:click={go}>go!</button> -->
-	<!-- <button on:click={go}>go!</button> -->
-	<button on:click|preventDefault={go}>go!</button>
-	<br />
-	{#if rr}
-		{#await fetchImage}
-			<p>...waiting</p>
-		{:then data}
-			<img src={data.message} alt="Dog image" />
-			<img {src} alt="Dog image" />
-		{:catch error}
-			<p>An error occurred!</p>
-		{/await}
-	{/if}
 
+	<button on:click={go}>go!</button>
+
+	<br />
+	<!-- {#if rr} -->
+	{#await fetchImage}
+		<p>...waiting</p>
+	{:then data}
+		<!-- {#each data as src} -->
+		<!-- svelte-ignore a11y-img-redundant-alt -->
+		<img {src} alt="Dog image" />
+		<p />
+		<img src={src1} alt="Doglkimage" />
+		<p />
+		<!-- 	<img src={data.message} alt="Dog image" /> -->
+
+		<!-- {/each} -->
+	{:catch error}
+		<p>An error occurred!</p>
+	{/await}
+
+	<!-- 	<Nested {onClose} /> -->
+	<!-- {/if} -->
+	<!-- {#if rrr}
+		<h1>ÖÖÖÖÖÖÖÖÖÖÖÖÖ</h1>
+	{/if} -->
 	<a href="/">back</a>
 </div>
 
